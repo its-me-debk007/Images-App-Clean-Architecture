acfed21d7ffd41ee0fcdc21632f7c3a9cac13b74
Debashish Kundu <debashish.joy@gmail.com>
2023-01-14 21:40:53 +0530

    added search feature

--- app/src/main/java/in/images/app/presentation/screens/HomeScreen.kt
+++ app/src/main/java/in/images/app/presentation/screens/HomeScreen.kt
@@ -1,14 +1,13 @@
 package `in`.images.app.presentation.screens
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,36 +17,53 @@ import com.bumptech.glide.integration.compose.GlideImage
 import `in`.images.app.common.ApiState
 import `in`.images.app.presentation.viewmodel.ImageViewModel
 
-@OptIn(ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: ImageViewModel = hiltViewModel()) {
     val result = viewModel.imageList.value
+    var query by remember { mutableStateOf("") }
 
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
+    Column(Modifier.fillMaxSize()) {
+        OutlinedTextField(
+            value = query,
+            onValueChange = {
+                query = it
+                viewModel.getImages(query.lowercase().trim())
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+
         when (result) {
             is ApiState.Loading -> {
-
-                CircularProgressIndicator()
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    CircularProgressIndicator()
+                }
             }
 
             is ApiState.Error -> {
-
-                Text(text = result.errorMsg.toString())
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = result.errorMsg.toString())
+                }
             }
 
             is ApiState.Success -> {
                 Log.d("API_Result", result.data.toString())
 
-                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
                     items(result.data!!.size) {
 
                         GlideImage(
                             model = result.data[it].imageUrl,
                             contentDescription = null,
-                            modifier = Modifier.fillMaxWidth()
+                            modifier = Modifier
+                                .fillMaxWidth()
                                 .padding(8.dp)
                         )
                     }
